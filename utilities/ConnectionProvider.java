a2f8b41677922e58689ec62e495c713ce20bc348
maikenapuada <43075235+maikenapuada@users.noreply.github.com>
2022-03-11 23:49:04 -0500

    Update ConnectionProvider.java

--- utilities/ConnectionProvider.java
+++ utilities/ConnectionProvider.java
@@ -2,7 +2,11 @@ package utilities;
 import java.sql.*;
 
 public class ConnectionProvider {
-	public static Connection getCon(String url, String user, String password) {
+	public static Connection getCon() {
+	//add database info here
+	String url="";
+	String user="";
+	String password="";
       try{
            Connection connection = DriverManager.getConnection(url,user,password);
            return connection;
