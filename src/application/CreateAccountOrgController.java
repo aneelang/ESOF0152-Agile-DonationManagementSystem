9d312e5afcec13519f94f57b19f81859d747b16f
k-fetus <62576481+k-fetus@users.noreply.github.com>
2022-03-14 18:29:56 -0400

    Add files via upload

---
+++ src/application/CreateAccountOrgController.java
@@ -0,0 +1,60 @@
+package application;
+
+import javafx.fxml.*;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CreateAccountOrgController implements Initializable {
+
+	public CreateAccountOrgController() {
+		
+	}
+	
+	//Create instances of variables of scene builder
+	@FXML
+	private Button createAccountButton;
+	@FXML
+	private TextField firstName;
+	@FXML
+	private TextField lastName;
+	@FXML
+	private TextField firstemailAddressName;
+	@FXML
+	private PasswordField password;
+	@FXML
+	public ComboBox<String> typeOfAccountBox;
+	String choice;
+	
+	ObservableList<String> list = FXCollections.observableArrayList("Volunteer", "Organization");
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		typeOfAccountBox.setItems(list);	
+	}
+	
+	public void comboChanged(ActionEvent event) throws IOException {
+		Main m = new Main();
+		if (typeOfAccountBox.getValue() == "Organization" ) {
+			System.out.println("Working Organization");
+		}
+		if (typeOfAccountBox.getValue() == "Volunteer" ) {
+			System.out.println("Working Volunteer");
+			m.changeScene("createAccountVolunteer.fxml");
+		}
+	}
+	
+	public void createAccount(ActionEvent event) throws IOException{
+		Main m = new Main();
+		System.out.println("Create Account button working.");
+		m.changeScene("Login.fxml");
+	}	
+}
